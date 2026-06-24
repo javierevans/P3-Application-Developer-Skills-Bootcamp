8d3c845b0c931e53bf5d3d7f14beb08986758fc4
Javier Evans <javierevans509@gmail.com>
2026-06-24 02:42:57 -0400

    Add tournament creation and persistence

---
+++ screens/tournaments/create.py
@@ -0,0 +1,23 @@
+from ..base_screen import BaseScreen
+from commands import TournamentCreateCmd
+
+class TournamentCreate(BaseScreen):
+    """Screen displayed when creating a tournament"""
+    
+    display = "## Create Tournament"
+
+    def get_command(self):
+        print("Type in the name of the Tournament")
+        name = self.input_string()
+        print("Type in the location of the Tournament")
+        location = self.input_string()
+        print("Type in the start date of the Tournament")
+        start_date = self.input_string()
+        print("Type in the end date of the Tournament")
+        end_date = self.input_string()
+        print("Type in the description of the Tournament")
+        description = self.input_string()
+        print("Type in the number of the rounds")
+        number_of_rounds = self.input_string()
+
+        return TournamentCreateCmd(name, location, start_date, end_date, description, number_of_rounds, )
